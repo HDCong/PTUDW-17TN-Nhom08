1e9c74dba22d8013c5b4451a0b155d47d667992e
khactrieu99 <khactrieu.personal@gmail.com>
2020-06-17 20:32:17 +0700

    update navbar

--- static-web/js/login-register/OnLoad.js
+++ static-web/js/login-register/OnLoad.js
@@ -83,11 +83,11 @@ function setNavbarFixed() {
     if(window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
         document.getElementsByClassName("main-content")[0].style.marginTop = `${navbar.clientHeight}px`;
-        // document.getElementsByClassName("nav-bar")[0].style.backgroundColor = '#12263A'
+        document.getElementById("content").style.boxShadow = '-1px 14px 37px -4px rgba(18,38,58,1)'
     }
     else {
         navbar.classList.remove("sticky");
         document.getElementsByClassName("main-content")[0].style.marginTop = '0px';
-        // document.getElementsByClassName("nav-bar")[0].style.backgroundColor = 'white'
+        document.getElementById("content").style.boxShadow = 'none'
     }
 }
